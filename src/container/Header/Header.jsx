e40479541a02983a352282645f898a0de4c064a1
Arghyadeep Bhowmick <bhowmickarghyadeep2@gmail.com>
2023-08-14 12:12:07 +0530

    Header section completed

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -17,7 +17,7 @@ const scaleVariants = {
 
 function Header() {
   return (
-    <div className="app__header app__flex">
+    <div className="app__header app__flex" id="home">
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 0.5 }}
@@ -56,7 +56,7 @@ function Header() {
       <motion.div
         variants={scaleVariants}
         whileInView={scaleVariants.whileInView}
-        className="app__header-circle"
+        className="app__header-circles"
       >
         {[images.flutter, images.redux, images.sass].map((circle, index) => (
           <div className="circle-cmp app__flex" key={`circle-${index}`}>
