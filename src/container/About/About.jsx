e113b53b709c0a4a61b521f3872320e015cc0c9a
Arghyadeep Bhowmick <bhowmickarghyadeep2@gmail.com>
2023-09-06 23:35:58 +0530

    Skills section created

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,26 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 
-import { images } from "../../constants";
-
+import { AppWrap } from "../../wrapper";
 import "./About.scss";
-
-const abouts = [
-  {
-    title: "Web Developer",
-    description:
-      "I am budding web developer with keen interest in mastering both frontend and backend.",
-    imgUrl: images.about01,
-  },
-  {
-    title: "Software Engineer",
-    description:
-      "I am an enthusiastic problem solver on the path to becoming a skilled software developer.",
-    imgUrl: images.about02,
-  },
-];
+import { urlFor, client } from "../../client";
 
 function About() {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => setAbouts(data));
+  }, []);
   return (
     <>
       <h2 className="head-text">
@@ -38,7 +30,7 @@ function About() {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>
               {about.title}
             </h2>
@@ -52,4 +44,4 @@ function About() {
   );
 }
 
-export default About;
+export default AppWrap(About, "about");
